6d64b2e15ec0f945da65a19a2ee6fc31fe671d60
Matias Grunberg <matias@yellowspot.dev>
2022-10-21 17:20:05 -0300

    use vite to add support for esm

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    minify: false,
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      fileName: 'index',
+      formats: ["cjs", "es"],
+    },
+    rollupOptions: {
+      external: ['vite', 'node:fs']
+    }
+  }
+})
